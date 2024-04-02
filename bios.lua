4b0d680aa340bd0d1922c3cd4724eead808b9245
yabastar0 <163224683+yabastar0@users.noreply.github.com>
2024-04-01 21:28:39 -0500

    Update bios.lua

--- bios.lua
+++ bios.lua
@@ -1,5 +1,5 @@
 local cDir = ""
-
+os.loadAPI("/rom/apis/io.lua")
 local needRestart = fs.open("horizon/sys/res.sys","r")
 local dbrestart = false
 if needRestart ~= nil then
@@ -35,17 +35,21 @@ local function update(link, name, bios)
 end
 
 local function updateAll()
+    update("https://raw.githubusercontent.com/yabastar0/horizonOS/main/tree.lua","tree",false)
+    update("https://raw.githubusercontent.com/yabastar0/horizonOS/main/type.lua","type",false)
+    update("https://raw.githubusercontent.com/yabastar0/horizonOS/main/mkdir.lua","mkdir",false)
+    update("https://raw.githubusercontent.com/yabastar0/horizonOS/main/ren.lua","ren",false)
+    update("https://raw.githubusercontent.com/yabastar0/horizonOS/main/del.lua","del",false)
+    update("https://raw.githubusercontent.com/yabastar0/horizonOS/main/rmdir.lua","rmdir",false)
     update("https://raw.githubusercontent.com/yabastar0/horizonOS/main/cd.lua","cd",false)
+    update("https://raw.githubusercontent.com/yabastar0/horizonOS/main/dir.lua","dir",false)
+    update("https://raw.githubusercontent.com/yabastar0/horizonOS/main/copy.lua","copy",false)
     update("https://raw.githubusercontent.com/yabastar0/horizonOS/main/bios.lua","bios",true)
 end
 
 if dbrestart == true then
     updateAll()
     os.reboot()
-end
-
-if fs.exists("horizon/cd") == false then
-    update("https://raw.githubusercontent.com/yabastar0/horizonOS/main/cd.lua","cd",false)
 end
 
 function hsh.addpath(path)
@@ -61,48 +65,54 @@ function hsh.setDir(path)
     cDir = fs.combine(path, "")
 end
 
+function hsh.getDir()
+    return cDir
+end
+
 function hsh.run(command)
     words = {}
     for word in command:gmatch("%S+") do table.insert(words, word) end
-    local cmd1 = words[1]
-    if fs.exists("horizon/"..fs.combine(cmd1)) == true then
-        table.remove(words,1)
-        local tmp = {}
-        for _,v in pairs(words) do
-            table.insert(tmp,v)
+    if words[1] ~= nil then
+        local cmd1 = words[1]
+        if fs.exists("horizon/"..fs.combine(cmd1)) == true then
+            table.remove(words,1)
+            local tmp = {}
+            for _,v in pairs(words) do
+                table.insert(tmp,v)
+            end
+            _G.argv = tmp
+            loadfile("horizon/"..fs.combine(cmd1))()
+        elseif fs.exists(fs.combine(cmd1)) then
+            table.remove(words,1)
+            local tmp = {}
+            for _,v in pairs(words) do
+                table.insert(tmp,v)
+            end
+            _G.argv = tmp
+            loadfile(fs.combine(cmd1))()
+        elseif cmd1 == "download" then
+            updateAll()
+            io.write("The system needs to restart for changes to effect. Restart now? (y/n) ")
+            local inp = ""
+            repeat
+                inp = io.read()
+            until inp == "y" or inp == "yes" or inp == "n" or inp == "no"
+            if inp == "y" or inp == "yes" then
+                local needdb = fs.open("horizon/sys/res.sys","w")
+                needdb.write("true")
+                needdb.close()
+                os.reboot()
+            end
+        else
+            print(textutils.serialise(words))
         end
-        _G.argv = tmp
-        loadfile("horizon/"..fs.combine(cmd1))()
-    elseif fs.exists(fs.combine(cmd1)) then
-        table.remove(words,1)
-        local tmp = {}
-        for _,v in pairs(words) do
-            table.insert(tmp,v)
-        end
-        _G.argv = tmp
-        loadfile(fs.combine(cmd1))()
-    elseif cmd1 == "download" then
-        updateAll()
-        io.write("The system needs to restart for changes to effect. Restart now? (y/n) ")
-        local inp = ""
-        repeat
-            inp = io.read()
-        until inp == "y" or inp == "yes" or inp == "n" or inp == "no"
-        if inp == "y" or inp == "yes" then
-            local needdb = fs.open("horizon/sys/res.sys","w")
-            needdb.write("true")
-            needdb.close()
-            os.reboot()
-        end
-    else
-        print(textutils.serialise(words))
     end
 end
 
 hsh.clear()
 
 while true do
-    io.write(cDir..">")
+    term.write(cDir..">")
     local command = io.read()
     hsh.run(command)
 end
